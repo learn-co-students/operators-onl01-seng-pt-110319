28f23f22bb5e23badd077f1abb8d49038ef72bac
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-11-08 19:12:27 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
+
 def unsafe?(speed)
-
+if speed > 60
+  return true
+end 
+if speed < 40
+  return true 
+else speed
+  return false
 end
-
-
+end 
 
 def not_safe?(speed)
-	
+  speed < 40 || speed > 60 ? true : false
 end
-	
+
+
+
+
+
 
 
