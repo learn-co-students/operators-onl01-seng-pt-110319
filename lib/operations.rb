9c739771786a018a786eb947d3766184ef8a1a67
qiong sun <joannesun44@gmail.com>
2019-11-12 19:59:46 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,32 @@
-def unsafe?(speed)
 
+def unsafe?(speed = 79 )
+  puts "true if speed is greater than 60."
 end
 
-
-
-def not_safe?(speed)
-	
+def unsafe?(speed = 35 )
+  puts "true if speed is less than 40."
 end
+
+def unsafe?(speed = 50 )
+  puts "false if the speed is between 40 and 60."
+end
+
 	
+def not_safe?(speed = 79)
+	puts "true if speed is greater than 60."
+end
 
+def not_safe?(speed = 35)
+	puts "true if speed is less than 40."
+end
 
+def not_safe?(speed = 50)
+	puts "false if the speed is between 40 and 60."
+end
+
+(speed = 79) not_safe? "True" if 60 < speed < 40 : "False" if 40 < speed < 60
+  
+(speed = 35) not_safe? "True" if 60 < speed < 40 : "False" if 40 < speed < 60
+
+(speed = 50) not_safe? "True" if 60 < speed < 40 : "False" if 40 < speed < 60
+    
